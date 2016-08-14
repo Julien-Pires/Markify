f0111bc83a30b1a524427767ca9629789c2cc7d7
Julien Pires <piresjulien95@gmail.com>
2016-08-14 20:35:59 +0200

    Add a hash builder expression

--- src/src/Markify.Core.FSharp/IOBuilder.fs
+++ src/src/Markify.Core.FSharp/IOBuilder.fs
@@ -21,3 +21,44 @@ type IOBuilder() =
 
     member this.Return x =
         Success x
+
+[<Struct>]
+type HashResult (hash : int, hasValue : bool, isOriginal : bool) =
+    member this.Hash with get() = hash
+    member internal this.HasValue with get() = hasValue
+    member internal this.IsOriginal with get() = isOriginal
+
+type HashBuilder(prime : int) =
+    let prime = prime
+    let identityHash = HashResult(1, true, true)
+    let noHash = HashResult(1, false, false)
+
+    let computeHash (a : HashResult) (b : HashResult) =
+        HashResult (b.Hash * prime + a.Hash, true, false)
+
+    member this.Zero() =
+        noHash
+    
+    member this.YieldFrom (c : 'a option) =
+        match c with
+        | Some x -> HashResult(x.GetHashCode(), true, true)
+        | None -> noHash
+        
+    member this.Yield (c) =
+        HashResult(c.GetHashCode(), true, true)
+
+    member this.Combine (a : HashResult, b : HashResult) =
+        match a.HasValue, b.HasValue with
+        | true, false -> a
+        | false, true -> b
+        | false, false -> noHash
+        | _ -> 
+            match b.IsOriginal with
+            | true -> 
+                identityHash
+                |> computeHash b 
+                |> computeHash a
+            | false -> computeHash a b
+
+    member this.Delay (f) =
+        f()
