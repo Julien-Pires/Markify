2958be266836c5ab166c6312c06805fafc91db56
Julien Pires <jpires@enablon.net>
2016-09-01 11:34:19 +0200

    Refactored SolutionExplorer tests customization

--- src/tests/Markify.Core.Tests/Attributes/SolutionExplorerCustomization.cs
+++ src/tests/Markify.Core.Tests/Attributes/SolutionExplorerCustomization.cs
@@ -14,8 +14,8 @@ namespace Markify.Core.Tests.Attributes
     {
         #region Fields
 
-        private readonly Mock<IIDEEnvironment> _ide;
-        private readonly Mock<ISolutionExplorerFilterProvider> _filterProvider;
+        private readonly IIDEEnvironment _ide;
+        private readonly ISolutionExplorerFilterProvider _filterProvider;
 
         #endregion
 
@@ -31,49 +31,65 @@ namespace Markify.Core.Tests.Attributes
             IEnumerable<ProjectLanguage> filteredLanguages,
             IEnumerable<string> filteredFiles)
         {
-            _ide = new Mock<IIDEEnvironment>();
-            _ide.SetupGet(c => c.CurrentSolution).Returns(solution);
-
-            var solutionPath = (Uri)null;
-            if (root != null)
-            {
-                if(!root.EndsWith("/"))
-                    root += "/";
-
-                solutionPath = new Uri(root);
-            }
-
-            _ide.Setup(c => c.GetSolutionPath(It.Is<string>(d => d == solution))).Returns(solutionPath);
-
-            var projects = Enumerable.Range(0, projectsCount).Select(c => $"Project{c + 1}").ToArray();
-            _ide.Setup(c => c.GetProjects(It.Is<string>(d => d == solution))).Returns(projects);
-
-            _ide.Setup(c => c.GetProjectPath(It.IsAny<string>(), It.IsIn(projects)))
-                .Returns<string, string>((s, p) => solutionPath != null ? new Uri(solutionPath, $"{p}/") : null);
-
-            _ide.SetupGet(c => c.CurrentProject)
-                .Returns(() => currentProject > -1 ? projects.ElementAt(currentProject) : null);
-
-            var fileExt = language == ProjectLanguage.CSharp ? "cs" : "vb";
-            var files = Enumerable.Range(0, filesPerProject).Select(c => $"File{c + 1}.{fileExt}");
-            _ide.Setup(c => c.GetProjectFiles(It.IsAny<string>(), It.IsIn(projects)))
-                .Returns<string, string>((s, p) => files.Select(f => new Uri(_ide.Object.GetProjectPath(It.IsAny<string>(), p), f)));
-
-            _ide.Setup(c => c.GetProjectLanguage(It.IsAny<string>(), It.IsIn(projects)))
-                .Returns<string, string>((s, p) => language);
-
-            _filterProvider = new Mock<ISolutionExplorerFilterProvider>();
-            _filterProvider.SetupGet(c => c.Filters)
-                           .Returns(() => new SolutionExplorerFilter(filteredLanguages, filteredFiles));
+            _ide = CreateIDEEnvironment(solution, root, projectsCount, currentProject, filesPerProject, language);
+            _filterProvider = CreateExplorerFilter(filteredLanguages, filteredFiles);
         }
 
         #endregion
 
         #region Customize
 
+        private static IIDEEnvironment CreateIDEEnvironment(string solution, string root, int projectsCount, int currentProject,
+            int filesPerProject, ProjectLanguage language)
+        {
+            var ide = new Mock<IIDEEnvironment>();
+            ide.SetupGet(c => c.CurrentSolution).Returns(solution);
+
+            var solutionPath = (Uri)null;
+            if (root != null)
+            {
+                if (!root.EndsWith("/"))
+                    root += "/";
+
+                solutionPath = new Uri(root);
+            }
+
+            ide.Setup(c => c.GetSolutionPath(It.Is<string>(d => d == solution))).Returns(solutionPath);
+
+            var projects = Enumerable.Range(0, projectsCount).Select(c => $"Project{c + 1}").ToArray();
+            ide.Setup(c => c.GetProjects(It.Is<string>(d => d == solution))).Returns(projects);
+
+            ide.Setup(c => c.GetProjectPath(It.IsAny<string>(), It.IsIn(projects)))
+                .Returns<string, string>((s, p) => solutionPath != null ? new Uri(solutionPath, $"{p}/") : null);
+
+            ide.SetupGet(c => c.CurrentProject)
+                .Returns(() => currentProject > -1 ? projects.ElementAt(currentProject) : null);
+
+            var fileExt = language == ProjectLanguage.CSharp ? "cs" : "vb";
+            var files = Enumerable.Range(0, filesPerProject).Select(c => $"File{c + 1}.{fileExt}");
+            ide.Setup(c => c.GetProjectFiles(It.IsAny<string>(), It.IsIn(projects)))
+                .Returns<string, string>((s, p) => files.Select(f => new Uri(ide.Object.GetProjectPath(It.IsAny<string>(), p), f)));
+
+            ide.Setup(c => c.GetProjectLanguage(It.IsAny<string>(), It.IsIn(projects)))
+                .Returns<string, string>((s, p) => language);
+
+            return ide.Object;
+        }
+
+        private static ISolutionExplorerFilterProvider CreateExplorerFilter(IEnumerable<ProjectLanguage> filteredLanguages, 
+            IEnumerable<string> filteredFiles)
+        {
+            var filterProvider = new Mock<ISolutionExplorerFilterProvider>();
+            filterProvider.SetupGet(c => c.Filters)
+                           .Returns(() => new SolutionExplorerFilter(filteredLanguages, filteredFiles));
+
+            return filterProvider.Object;
+        }
+
         public void Customize(IFixture fixture)
         {
-            fixture.Register(() => new SolutionExplorer(_ide.Object, _filterProvider.Object));
+            fixture.Inject(_ide);
+            fixture.Inject(_filterProvider);
         }
 
         #endregion
