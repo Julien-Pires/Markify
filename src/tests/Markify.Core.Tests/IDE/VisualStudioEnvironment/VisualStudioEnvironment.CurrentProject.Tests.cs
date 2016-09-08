377bc247ba58ab873effb49a08d66a15cca1296e
Julien Pires <jpires@enablon.net>
2016-09-08 15:05:56 +0200

    Add new tests organization in Core tests project

---
+++ src/tests/Markify.Core.Tests/IDE/VisualStudioEnvironment/VisualStudioEnvironment.CurrentProject.Tests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Markify.Core.IDE.VisualStudio;
+using Markify.Core.Tests.Attributes;
+using NFluent;
+using Xunit;
+
+namespace Markify.Core.Tests.IDE
+{
+    public sealed partial class VisualStudioEnvironment_Tests
+    {
+        [Theory]
+        [VisualStudioEnvironmentData("Foo", 2, 0, false)]
+        public void CurrentProject_ShouldReturnNull_WhenHasNoActiveProject(VisualStudioEnvironment sut)
+        {
+            Check.That(sut.CurrentProject).IsNull();
+        }
+
+        [Theory]
+        [VisualStudioEnvironmentData("Foo", 0, 0, true)]
+        public void CurrentProject_ShouldReturnNull_WhenSolutionHasNoProject(VisualStudioEnvironment sut)
+        {
+            Check.That(sut.CurrentProject).IsNull();
+        }
+
+        [Theory]
+        [VisualStudioEnvironmentData("Foo", 2, 0, true)]
+        public void CurrentProject_ShouldReturnProjectName_WhenHasActiveProject(VisualStudioEnvironment sut)
+        {
+            Check.That(sut.CurrentProject).IsNotNull();
+        }
+
+        [Theory]
+        [VisualStudioEnvironmentData("Foo", 2, 0, true, "Foo")]
+        public void CurrentProject_ShouldReturnProjectNameFromCurrentSolution_WhenHasActiveProject(string solution, VisualStudioEnvironment sut)
+        {
+            var projects = sut.GetProjects(solution);
+
+            Check.That(projects.Contains(sut.CurrentProject)).IsTrue();
+        }
+    }
+}
