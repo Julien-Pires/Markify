d49c035e190c3521a3a089a6a9485a3117b24bca
Julien Pires <piresjulien95@gmail.com>
2016-09-20 23:13:13 +0200

    Update models implementation

--- src/src/Markify.Models/Definitions.fs
+++ src/src/Markify.Models/Definitions.fs
@@ -1,6 +1,5 @@
 ﻿namespace Markify.Models.Definitions
 
-open Markify.Core.Builders
 open Markify.Models.IDE
 
 type StructureKind =  
@@ -12,44 +11,26 @@ type StructureKind =
     | Enum = 5
 
 type DefinitionName = string
-
-[<CustomEquality; NoComparison>]
-type DefinitionIdentity =
-  { Name : DefinitionName
+type DefinitionIdentity = {
+    Name : DefinitionName
     Parents : DefinitionName option
     Namespace : DefinitionName option }
 
-    override this.Equals(c) =
-        match c with
-        | :? DefinitionIdentity as x ->
-            this.Name = x.Name && this.Parents = x.Parents && this.Namespace = x.Namespace
-        | _ -> false
-
-    override this.GetHashCode() =
-        let result = HashBuilder(31){
-            yield this.Name
-            yield! this.Parents
-            yield! this.Namespace
-        }
-        result.Hash
-
 type Modifier = string
-type ConstraintsList = string seq
+type Constraint = string
 type GenericParameterDefinition = {
     Identity : DefinitionName
     Modifier : Modifier
-    Constraints : ConstraintsList }
+    Constraints : Constraint seq }
 
-type ModifiersList = Modifier seq
-type BaseTypesList = string seq
-type GenericParametersList = GenericParameterDefinition seq
+type BaseType = string
 type TypeDefinition = {
     Identity : DefinitionIdentity
     Kind : StructureKind
-    AccessModifiers : ModifiersList
-    Modifiers : ModifiersList
-    BaseTypes : BaseTypesList
-    Parameters : GenericParametersList }
+    AccessModifiers : Modifier seq
+    Modifiers : Modifier seq
+    BaseTypes : BaseType seq
+    Parameters : GenericParameterDefinition seq }
 
 type NamespaceDefinition = {
     Name : DefinitionName }
