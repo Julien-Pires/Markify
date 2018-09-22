54ed6c140bbb2a487e603b0677c588752c0afeb3
Julien Pires <piresjulien95@gmail.com>
2018-09-22 14:22:04 +0200

    Update testTheory and testRepeat

--- src/Services/Roslyn/Markify.Services.Roslyn.Tests/Fixtures.fs
+++ src/Services/Roslyn/Markify.Services.Roslyn.Tests/Fixtures.fs
@@ -27,11 +27,11 @@ module Fixtures =
     let testRepeat setup values tests =
         tests
         |> Seq.map (fun (name, c) ->
-            values |> Seq.map (fun value -> Tests.testCase (sprintf "%s - (%A)" name value) (setup value c)))
+            values |> Seq.map (fun value -> Tests.testCase (sprintf "%s - %A" name value) (setup value c)))
         |> Seq.collect id
 
     let testTheory name values test = 
-        values |> Seq.map (fun c -> (sprintf "%s (%A)" name c, test c))
+        values |> Seq.map (fun c -> (sprintf "%s / %A" name c, test c))
 
     let withProject name language f () = 
         f <|| (ProjectBuilder.create name language, (RoslynAnalyzer(modules) :> IProjectAnalyzer))
