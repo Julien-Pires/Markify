1dbd099bf938986e3b5f4d31abdd468ea3aa2216
Julien Pires <piresjulien95@gmail.com>
2018-09-23 17:25:22 +0200

    Update coveralls script

--- coveralls.ps1
+++ coveralls.ps1
@@ -1,7 +1,7 @@
 $coverageFilePath = Resolve-Path -path "TestResults\*\*.coverage"
 $coverageFilePath = $coverageFilePath.ToString()
 
-."C:\Program Files (x86)\Microsoft Visual Studio 12.0\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.xml "$coverageFilePath"
+CodeCoverage.exe analyze /output:coverage.xml "$coverageFilePath"
 
 csmacnz.coveralls --dynamiccodecoverage -i coverage.xml `
     --repoToken $env:COVERALLS_REPO_TOKEN `
