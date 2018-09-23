3f88fc5183fbda9e733d2f329efbccfda6fa1d2d
Julien Pires <piresjulien95@gmail.com>
2018-09-23 18:04:02 +0200

    Fix typo in scripts

--- coveralls.ps1
+++ coveralls.ps1
@@ -4,7 +4,7 @@ $coverageFilePath = $coverageFilePath.ToString()
 ."C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.xml "$coverageFilePath"
 
 dotnet tool install -g coveralls.net --version 1.0.0
-csmacnz.coveralls --dynamiccodecoverage -i coverage.xml `
+csmacnz.Coveralls --dynamiccodecoverage -i coverage.xml `
     --repoToken $env:COVERALLS_REPO_TOKEN `
     --commitId $env:Build.SourceVersion `
     --commitBranch $env:Build.SourceBranch `
