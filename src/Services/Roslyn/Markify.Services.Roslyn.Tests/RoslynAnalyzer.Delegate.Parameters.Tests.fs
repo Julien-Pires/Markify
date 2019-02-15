c35c4e21e3d9b19287e681f2eb81a6b751a4f918
Julien Pires <piresjulien95@gmail.com>
2019-02-16 00:04:17 +0100

    Re-write delegate parameter tests

--- src/Services/Roslyn/Markify.Services.Roslyn.Tests/RoslynAnalyzer.Delegate.Parameters.Tests.fs
+++ src/Services/Roslyn/Markify.Services.Roslyn.Tests/RoslynAnalyzer.Delegate.Parameters.Tests.fs
@@ -1,94 +1,199 @@
 ﻿namespace Markify.Services.Roslyn.Tests
 
-module RoslynAnalyzerDelegateParametersTests =
-    open Markify.Services.Roslyn
-    open Markify.Domain.Ide
-    open Markify.Domain.Compiler
-    open Swensen.Unquote
-    open Xunit
+open Markify.Domain.Compiler
+open Expecto
+open Swensen.Unquote
+open Fixtures
 
-    [<Theory>]
-    [<ProjectData("Delegates", "WithoutParameters")>]
-    let ``Analyze should return no parameters when delegate has none`` (name, sut : RoslynAnalyzer, project) =
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual =
-            TestHelper.getDefinition name library 
-            |> DefinitionsHelper.getParameters
+module RoslynAnalyzer_DelegateParameters_Tests =
+    [<Tests>]
+    let noParametersTests =
+        let content = [
+            (
+                ProjectLanguage.CSharp,
+                ["
+                    public delegate void WithoutParameters();
+                "]
+            )
+            (
+                ProjectLanguage.VisualBasic, 
+                ["
+                    Public Delegate Sub WithoutParameters()
+                "]
+            )
+        ]
+        testList "Analyze/Delegate" [
+            yield! testRepeat (withProjects content)
+                "should return no parameters when delegate has none"
+                (fun sut project () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies "WithoutParameters"
 
-        <@ actual |> Seq.isEmpty @>
+                    test <@ result.Parameters |> Seq.isEmpty @>)
+        ]
 
-    [<Theory>]
-    [<ProjectData("Delegates", "WithOneParameter")>]
-    [<ProjectData("Delegates", "WithMultipleParameters")>]
-    let ``Analyze should return parameters when delegate has some`` (name, sut : RoslynAnalyzer, project) =
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual = 
-            TestHelper.getDefinition name library 
-            |> DefinitionsHelper.getParameters
+    [<Tests>]
+    let withParametersTests =
+        let content = [
+            (
+                ProjectLanguage.CSharp,
+                ["
+                    public delegate void WithOneParameter(Int32 foo);
+                    public delegate void WithMultipleParameters(Int32 foo, Single bar);
+                    public delegate void WithGenericParameters<T>(T foo, T[] bar);
+                "]
+            )
+            (
+                ProjectLanguage.VisualBasic, 
+                ["
+                    Public Delegate Sub WithOneParameter(foo As Int32)
+                    Public Delegate Sub WithMultipleParameters(foo As Int32, bar As Single)
+                    Public Delegate Sub WithGenericParameters(Of T)(foo As T, bar As T())
+                "]
+            )
+        ]
+        testList "Analyze/Delegate" [
+            yield! testRepeatParameterized
+                "should return parameters when delegate has some"[
+                ((withProjects content), ("WithOneParameter", 1))
+                ((withProjects content), ("WithMultipleParameters", 2))]
+                (fun sut project (name, expected) () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies name
 
-        <@ actual |> (Seq.isEmpty >> not) @>
+                    test <@ result.Parameters |> Seq.length = expected @>)
+
+            yield! testRepeatParameterized
+                "should return correct parameters name when delegate has some"[
+                ((withProjects content), ("WithOneParameter", "foo"))
+                ((withProjects content), ("WithMultipleParameters", "foo"))
+                ((withProjects content), ("WithMultipleParameters", "bar"))]
+                (fun sut project (name, expected) () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies name
+
+                    test <@ result.Parameters |> Seq.exists (fun c -> c.Name = expected) @>)
+
+            yield! testRepeatParameterized
+                "should return correct parameters return type when delegate has some"[
+                ((withProjects content), ("WithOneParameter", "foo", "Int32"))
+                ((withProjects content), ("WithMultipleParameters", "foo", "Int32"))
+                ((withProjects content), ("WithMultipleParameters", "bar", "Single"))
+                ((withProjects content), ("WithGenericParameters`1", "foo", "T"))]
+                (fun sut project (name, parameter, expected) () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies name
+
+                    test <@ result.Parameters |> Seq.find (fun c -> c.Name = parameter)
+                                              |> fun c -> c.Type = expected @>)
+        ]
+
+    [<Tests>]
+    let noParameterModifierTests =
+        let content = [
+            (
+                ProjectLanguage.CSharp,
+                ["
+                    public delegate void WithOneParameter(Int32 foo);
+                "]
+            )
+            (
+                ProjectLanguage.VisualBasic, 
+                ["
+                    Public Delegate Sub WithOneParameter(foo As Int32)
+                "]
+            )
+        ]
+        testList "Analyze/Delegate" [
+            yield! testRepeat (withProjects content)
+                "should return no modifier when delegate parameter has none"
+                (fun sut project () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies "WithOneParameter"
+
+                    test <@ result.Parameters |> Seq.find (fun c -> c.Name = "Foo")
+                                              |> fun c -> c.Modifier.IsNone @>)
+        ]
+
+    [<Tests>]
+    let withParameterModifierTests =
+        let content = [
+            (
+                ProjectLanguage.CSharp,
+                ["
+                    public delegate void WithParametersModifiers(ref Int32 foo, out Int32 bar);
+                "]
+            )
+            (
+                ProjectLanguage.VisualBasic, 
+                ["
+                    Public Delegate Sub WithParametersModifiers(ByRef foo As Int32, ByVal bar As Single)
+                "]
+            )
+        ]
+        testList "Analyze/Delegate" [
+            yield! testRepeatParameterized
+                "should return modifier when delegate parameter has one" [
+                ((withProjects content), ("WithParametersModifiers", "foo", "ref"))
+                ((withProjects content), ("WithParametersModifiers", "bar", "out"))]
+                (fun sut project (name, parameter, expected) () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies name
+
+                    test <@ result.Parameters |> Seq.find (fun c -> c.Name = parameter)
+                                              |> fun c -> c.Modifier = Some expected @>)
+        ]
     
-    [<Theory>]
-    [<ProjectData("Delegates", "WithOneParameter", "foo")>]
-    [<ProjectData("Delegates", "WithMultipleParameters", "bar")>]
-    let ``Analyze should return expected delegate parameter name`` (name, parameter, sut : RoslynAnalyzer, project) =
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual = 
-            TestHelper.getDefinition name library 
-            |> DefinitionsHelper.getParameters
+    [<Tests>]
+    let noParameterDefaultValueTests =
+        let content = [
+            (
+                ProjectLanguage.CSharp,
+                ["
+                    public delegate void WithOneParameter(Int32 foo);
+                "]
+            )
+            (
+                ProjectLanguage.VisualBasic, 
+                ["
+                    Public Delegate Sub WithOneParameter(foo As Int32)
+                "]
+            )
+        ]
+        testList "Analyze/Delegate" [
+            yield! testRepeat (withProjects content)
+                "should return no default value when delegate parameter has none"
+                (fun sut project () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies "WithOneParameter"
 
-        test <@ actual |> Seq.exists (fun c -> c.Name = parameter) @>
-    
-    [<Theory>]
-    [<ProjectData("Delegates", "WithMultipleParameters", "foo", "Int32")>]
-    [<ProjectData("Delegates", "WithMultipleParameters", "bar", "Single")>]
-    [<ProjectData("Delegates", "WithGenericParameters`1", "foo", "T")>]
-    let ``Analyze should return expected delegate parameter type`` (name, parameter, expected, sut : RoslynAnalyzer, project) =
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual = 
-            TestHelper.getDefinition name library 
-            |> TestHelper.getDelegateParameter parameter
+                    test <@ result.Parameters |> Seq.find (fun c -> c.Name = "Foo")
+                                              |> fun c -> c.DefaultValue.IsNone @>)
+        ]
 
-        <@ actual.Type = expected @>
-    
-    [<Theory>]
-    [<ProjectData("Delegates", "WithOneParameter", "foo")>]
-    let ``Analyze should return no modifier when delegate parameter has none`` (name, parameter, sut : RoslynAnalyzer, project) =
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual = 
-            TestHelper.getDefinition name library 
-            |> TestHelper.getDelegateParameter parameter
+    [<Tests>]
+    let withParameterDefaultValueTests =
+        let content = [
+            (
+                ProjectLanguage.CSharp,
+                ["
+                    public delegate void WithDefaultValueParameter(Int32 foo = 1);
+                "]
+            )
+            (
+                ProjectLanguage.VisualBasic, 
+                ["
+                    Public Delegate Sub WithDefaultValueParameter(Optional foo As Int32 = 1)
+                "]
+            )
+        ]
+        testList "Analyze/Delegate" [
+            yield! testRepeat (withProjects content)
+                "should return default value when delegate parameter has one"
+                (fun sut project () ->
+                    let assemblies = sut.Analyze project
+                    let result = findDelegate assemblies "WithDefaultValueParameter"
 
-        <@ actual.Modifier = None @>
-
-    [<Theory>]
-    [<ProjectData("Delegates", "WithParametersModifiers", "foo", "ref")>]
-    [<ProjectData("Delegates", "WithParametersModifiers", "foo", "out")>]
-    let ``Analyze should return modifiers when delegate parameter has some`` (name, parameter, modifier, sut : RoslynAnalyzer, project) =
-        let expected = LanguageHelperOld.getMemberModifiers project.Language modifier
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual = 
-            TestHelper.getDefinition name library 
-            |> TestHelper.getDelegateParameter parameter
-        
-        <@ actual.Modifier = Some expected @>
-    
-    [<Theory>]
-    [<ProjectData("Delegates", "WithOneParameter", "foo")>]
-    let ``Analyze should return no default value when delegate parameter has none`` (name, parameter, sut : RoslynAnalyzer, project) =
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual = 
-            TestHelper.getDefinition name library 
-            |> TestHelper.getDelegateParameter parameter
-
-        <@ actual.DefaultValue = None @>
-
-    [<Theory>]
-    [<ProjectData("Delegates", "WithDefaultParameters", "foo", "1")>]
-    let ``Analyze should return default value when delegate parameter has some`` (name, parameter, expected, sut : RoslynAnalyzer, project) =
-        let library = (sut :> IProjectAnalyzer).Analyze project
-        let actual = 
-            TestHelper.getDefinition name library 
-            |> TestHelper.getDelegateParameter parameter
-        
-        <@ actual.DefaultValue = Some expected @>
+                    test <@ result.Parameters |> Seq.find (fun c -> c.Name = "Foo")
+                                              |> fun c -> c.DefaultValue = Some "1" @>)
+        ]
